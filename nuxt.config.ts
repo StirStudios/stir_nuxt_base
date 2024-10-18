8800cae77f023808baa9e0d9c9c3c4e94f8477f1
Glyn Gray <glyn@stirstudiosdesign.com>
2024-10-17 18:23:11 -0700

    Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,13 +14,13 @@ export default defineNuxtConfig({
       }
     },
   },
-  routeRules: {
-    // General pages cached for 1 day using SWR
-    // '/**': { swr: 86400 },
-    '/admincontrol/login': {
-      redirect: `${process.env.DRUPAL_URL}/admincontrol/login`,
-    },
-  },
+  // routeRules: {
+  //   // General pages cached for 1 day using SWR
+  //   // '/**': { swr: 86400 },
+  //   '/admincontrol/login': {
+  //     redirect: `${process.env.DRUPAL_URL}/admincontrol/login`,
+  //   },
+  // },
   app: {
     head: {
       htmlAttrs: {
@@ -41,12 +41,12 @@ export default defineNuxtConfig({
         // disallow: ['/secret', '/admin'],
       },
     ],
-    [
-      '@nuxtjs/turnstile',
-      {
-        siteKey: `${process.env.TURNSTILE}`,
-      },
-    ],
+    // [
+    //   '@nuxtjs/turnstile',
+    //   {
+    //     siteKey: `${process.env.TURNSTILE}`,
+    //   },
+    // ],
     [
       '@nuxtjs/sitemap',
       {
@@ -61,88 +61,88 @@ export default defineNuxtConfig({
         exposeAPIRouteRules: true,
       },
     ],
-    [
-      'nuxt-gtag',
-      {
-        id: process.env.GTM,
-        initialConsent: true,
-        loadingStrategy: 'defer',
-      },
-    ],
-    [
-      'nuxt-booster',
-      {
-        disableNuxtFontaine: true,
-        disableNuxtImage: true,
+    //     [
+    //       'nuxt-gtag',
+    //       {
+    //         id: process.env.GTM,
+    //         initialConsent: true,
+    //         loadingStrategy: 'defer',
+    //       },
+    //     ],
+    //     [
+    //       'nuxt-booster',
+    //       {
+    //         disableNuxtFontaine: true,
+    //         disableNuxtImage: true,
+    //
+    //         detection: {
+    //           performance: true,
+    //           browserSupport: true,
+    //         },
+    //
+    //         performanceMetrics: {
+    //           device: {
+    //             hardwareConcurrency: { min: 2, max: 48 },
+    //             deviceMemory: { min: 2 },
+    //           },
+    //           timing: {
+    //             fcp: 800,
+    //             dcl: 1200,
+    //           },
+    //         },
 
-        detection: {
-          performance: true,
-          browserSupport: true,
-        },
+    // fonts: [
+    //   {
+    //     family: 'Vollkorn',
+    //     locals: ['Vollkorn'],
+    //     fallback: ['Georgia', 'Times New Roman', 'serif'],
+    //     variances: [
+    //       {
+    //         style: 'normal',
+    //         weight: 400,
+    //         sources: [
+    //           {
+    //             src: '@/assets/fonts/vollkorn-v23-latin-regular.woff2',
+    //             type: 'woff2',
+    //           },
+    //           {
+    //             src: '@/assets/fonts/vollkorn-v23-latin-regular.woff',
+    //             type: 'woff',
+    //           },
+    //         ],
+    //       },
+    //       {
+    //         style: 'normal',
+    //         weight: 500,
+    //         sources: [
+    //           {
+    //             src: '@/assets/fonts/vollkorn-v23-latin-500.woff2',
+    //             type: 'woff2',
+    //           },
+    //           {
+    //             src: '@/assets/fonts/vollkorn-v23-latin-500.woff',
+    //             type: 'woff',
+    //           },
+    //         ],
+    //       },
+    //     ],
+    //   },
+    // ],
 
-        performanceMetrics: {
-          device: {
-            hardwareConcurrency: { min: 2, max: 48 },
-            deviceMemory: { min: 2 },
-          },
-          timing: {
-            fcp: 800,
-            dcl: 1200,
-          },
-        },
-
-        // fonts: [
-        //   {
-        //     family: 'Vollkorn',
-        //     locals: ['Vollkorn'],
-        //     fallback: ['Georgia', 'Times New Roman', 'serif'],
-        //     variances: [
-        //       {
-        //         style: 'normal',
-        //         weight: 400,
-        //         sources: [
-        //           {
-        //             src: '@/assets/fonts/vollkorn-v23-latin-regular.woff2',
-        //             type: 'woff2',
-        //           },
-        //           {
-        //             src: '@/assets/fonts/vollkorn-v23-latin-regular.woff',
-        //             type: 'woff',
-        //           },
-        //         ],
-        //       },
-        //       {
-        //         style: 'normal',
-        //         weight: 500,
-        //         sources: [
-        //           {
-        //             src: '@/assets/fonts/vollkorn-v23-latin-500.woff2',
-        //             type: 'woff2',
-        //           },
-        //           {
-        //             src: '@/assets/fonts/vollkorn-v23-latin-500.woff',
-        //             type: 'woff',
-        //           },
-        //         ],
-        //       },
-        //     ],
-        //   },
-        // ],
-
-        componentAutoImport: false,
-        componentPrefix: undefined,
-
-        lazyOffset: {
-          component: '0%',
-          asset: '0%',
-        },
-      },
-    ],
+    //         componentAutoImport: false,
+    //         componentPrefix: undefined,
+    //
+    //         lazyOffset: {
+    //           component: '0%',
+    //           asset: '0%',
+    //         },
+    //       },
+    //     ],
   ],
   runtimeConfig: {
-    turnstile: {
-      secretKey: process.env.TURNSTILE_SECRET,
-    },
+    // turnstile: {
+    //   secretKey: process.env.TURNSTILE_SECRET,
+    // },
     api: process.env.DRUPAL_URL,
     public: {
       api: process.env.DRUPAL_URL,
