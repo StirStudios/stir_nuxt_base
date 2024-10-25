f3c13beb8922765e80bea296506896148cfc698b
Glyn Gray <glyn@stirstudiosdesign.com>
2024-10-25 16:03:13 -0700

    Sounded edit

--- app/components/Edit/EditLink.vue
+++ app/components/Edit/EditLink.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <UButton
-    class="absolute right-0 top-0 z-50 rounded-bl opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+    class="absolute right-0 top-0 z-50 rounded opacity-0 transition-opacity duration-300 group-hover:opacity-100"
     icon="i-heroicons-pencil-square"
     v-if="link"
     :to="link"
