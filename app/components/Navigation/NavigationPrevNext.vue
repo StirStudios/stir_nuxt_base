a34f8125675cfbfc70e29a5f8fe418cc5b31e2b4
Glyn Gray <glyn@stirstudiosdesign.com>
2024-11-02 13:48:45 -0700

    NextPrev Comp

---
+++ app/components/Navigation/NavigationPrevNext.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { PageProps } from '~/types/PageTypes'
+
+const page = ref<PageProps>
+</script>
+
+<template>
+  <template v-if="page.prevNext">
+    <UButton
+      v-if="page.prevNext.prev_node"
+      icon="i-heroicons-pencil-square"
+      size="sm"
+      color="primary"
+      square
+      variant="solid"
+      label="Previous"
+      :to="page.prevNext.prev_node.url"
+    />
+    <UButton
+      v-if="page.prevNext.next_node"
+      icon="i-heroicons-pencil-square"
+      size="sm"
+      color="primary"
+      square
+      variant="solid"
+      label="Next"
+      :to="page.prevNext.next_node.url"
+    />
+  </template>
+</template>
