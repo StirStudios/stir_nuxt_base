436bccecfdecb7b4417ad8b1cd923dfa6e2b7b10
Glyn Gray <glyn@stirstudiosdesign.com>
2024-11-03 14:53:03 -0800

    Updated Nav test

--- app/components/Navigation/NavigationMain.vue
+++ app/components/Navigation/NavigationMain.vue
@@ -1,25 +1,35 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ref } from 'vue'
 import { useDrupalApi } from '~/composables/useDrupalApi'
+import { useRoute } from '#app'
+import { useColorModeToggle } from '@nuxt/color-mode'
+
+// Fetch data from Drupal API
 const { page, isAdministrator, fetchMenu } = await useDrupalApi()
 const mainMenu = await fetchMenu('main')
 
-// Extract the raw array value from the reactive ref object 'mainMenu'
+// Extract the raw array value from the reactive ref object
 const mainMenuArray = mainMenu.value
 
-// Set color mode toggle.
+// Set color mode toggle
 const { isDark } = useColorModeToggle()
 
-// Map over the array of menu items to create navLinks
-const navLinks = mainMenuArray.map((menuItem) => ({
-  label: menuItem.title,
-  to: menuItem.external
-    ? menuItem.absolute
-    : `/${menuItem.alias}${menuItem.options && menuItem.options.fragment ? `#${menuItem.options.fragment}` : ''}`,
-}))
+// Map over the array of menu items to create navLinks in the format required by UNavigationMenu
+const navLinks = ref(
+  mainMenuArray.map((menuItem) => ({
+    label: menuItem.title,
+    icon: 'i-heroicons-arrow-right-20-solid', // Replace or customize as needed
+    to: menuItem.external
+      ? menuItem.absolute
+      : `/${menuItem.alias}${menuItem.options && menuItem.options.fragment ? `#${menuItem.options.fragment}` : ''}`,
+    children: menuItem.children?.map((child) => ({
+      label: child.title,
+      to: child.external ? child.absolute : `/${child.alias}`,
+    })),
+  })),
+)
 
 const showNavbar = ref(true)
-const navbarOpen = ref(false)
-const lastScrollPosition = ref(0)
 const isOpen = ref(false)
 const isScrolled = ref(false)
 const route = useRoute()
@@ -29,10 +39,7 @@ const onScroll = () => {
   const currentScrollPosition =
     window.pageYOffset || document.documentElement.scrollTop
   if (currentScrollPosition < 0) return
-  if (Math.abs(currentScrollPosition - lastScrollPosition.value) < 40) return
-  showNavbar.value = currentScrollPosition < lastScrollPosition.value
-  lastScrollPosition.value = currentScrollPosition
-  navbarOpen.value = false
+  showNavbar.value = currentScrollPosition < 50
   isScrolled.value = currentScrollPosition > 50
 }
 
@@ -59,54 +66,36 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-  <div
-    v-if="isAdministrator"
-    id="admin-menu"
-    aria-label="Admin navigation"
-    class="sticky top-0 z-50 h-[3.1rem]"
-    role="navigation"
-  />
   <header aria-label="Site header">
     <nav
       aria-label="Site navigation"
-      class="md:px-auto bg-opacity-90 dark:bg-opacity-70 fixed top-0 z-10 w-full bg-white px-4 px-8 py-3 shadow shadow-gray-300 backdrop-blur-md dark:bg-gray-950 dark:shadow-gray-700"
+      class="bg-opacity-90 dark:bg-opacity-70 fixed top-0 z-10 w-full bg-white px-4 py-3 shadow backdrop-blur-md dark:bg-gray-950 dark:shadow-gray-700"
       :class="{
         'navbar--hidden': !showNavbar,
-        sticky: isAdministrator && showNavbar,
       }"
       role="navigation"
     >
       <UContainer>
-        <div class="mx-auto flex flex-wrap items-center justify-between">
-          <div class="order-1">
-            <ULink aria-label="Site Logo" class="font-bold" to="/">
-              <template v-if="!page.site_info?.name">
-                <AppLogo />
-              </template>
-              <template v-else>
-                {{ page.site_info?.name }}
-              </template>
-            </ULink>
-          </div>
-          <div class="order-2 flex">
-            <UHorizontalNavigation
-              class="hidden border-none md:block"
-              :links="navLinks"
-              :ui="{
-                after: '',
-                container: 'block w-full sm:flex sm:items-center min-w-0',
-              }"
-            >
-              <template #default="{ link }">
-                <span
-                  class="group-hover:text-primary relative block w-full duration-500"
-                >
-                  {{ link.label }}
-                </span>
-              </template>
-            </UHorizontalNavigation>
-          </div>
-          <div class="order-2 flex md:order-3">
+        <div class="flex items-center justify-between">
+          <!-- Logo -->
+          <ULink aria-label="Site Logo" class="font-bold" to="/">
+            <template v-if="!page.site_info?.name">
+              <AppLogo />
+            </template>
+            <template v-else>
+              {{ page.site_info?.name }}
+            </template>
+          </ULink>
+
+          <!-- Desktop Navigation -->
+          <UNavigationMenu
+            :items="navLinks"
+            orientation="vertical"
+            color="neutral"
+            @click="isOpen = false"
+          />
+          <!-- Theme Toggle and Mobile Menu Button -->
+          <div class="flex items-center space-x-4">
             <ClientOnly>
               <UButton
                 aria-label="Theme"
@@ -121,15 +110,10 @@ onBeforeUnmount(() => {
               />
             </ClientOnly>
             <UButton
-              aria-label="Site navigation toggle"
-              class="block flex items-center md:hidden"
+              aria-label="Open mobile menu"
+              class="block md:hidden"
               color="gray"
-              :icon="
-                navbarOpen
-                  ? 'i-heroicons-x-mark-solid'
-                  : 'i-heroicons-bars-3-solid'
-              "
-              size="xl"
+              icon="i-heroicons-bars-3-solid"
               variant="ghost"
               @click="isOpen = true"
             />
@@ -137,47 +121,20 @@ onBeforeUnmount(() => {
         </div>
       </UContainer>
     </nav>
-    <USlideover
-      v-model="isOpen"
-      :ui="{
-        width: 'w-full max-w-full',
-      }"
-    >
-      <UCard
-        class="size-lg flex flex-1 flex-col"
-        :ui="{
-          body: { base: 'flex-1' },
-          ring: '',
-          divide: 'divide-y divide-gray-100 dark:divide-gray-800',
-        }"
-      >
-        <template #header>
-          <div class="flex items-center justify-between">
-            <ULink class="m-auto !outline-none" to="/" @click="isOpen = false">
-              <template v-if="page.site_info?.name">
-                <AppLogo />
-              </template>
-              <template v-else>
-                {{ page.site_info?.name }}
-              </template>
-            </ULink>
-            <UButton
-              class="-my-1"
-              color="gray"
-              icon="i-heroicons-x-mark-20-solid"
-              variant="ghost"
-              @click="isOpen = false"
-            />
-          </div>
-        </template>
-        <UVerticalNavigation
-          :links="navLinks"
-          :ui="{
-            base: 'my-3 uppercase',
-            label: 'text-center w-full',
-            padding: 'px-3.5 py-2.5',
-            size: 'text-lg',
-          }"
+
+    <!-- Mobile Navigation -->
+    <USlideover v-model="isOpen">
+      <UCard class="flex flex-col">
+        <UButton
+          class="self-end"
+          icon="i-heroicons-x-mark-20-solid"
+          variant="ghost"
+          @click="isOpen = false"
+        />
+        <UNavigationMenu
+          :items="navLinks"
+          orientation="vertical"
+          color="neutral"
           @click="isOpen = false"
         />
       </UCard>
