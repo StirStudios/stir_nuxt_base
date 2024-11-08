d47f96df4d5f1fd2f9b98ddb8d52337dc77e3709
Glyn Gray <glyn@stirstudiosdesign.com>
2024-11-08 14:26:33 -0800

    Correct import

--- app/components/Media/MediaPopup.vue
+++ app/components/Media/MediaPopup.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { MediaProps } from '@stir/base/types/MediaTypes'
+import type { MediaProps } from '~/types/MediaTypes'
 import { aspectRatio } from '~/utils/aspectRatio'
 
 const appConfig = useAppConfig()
