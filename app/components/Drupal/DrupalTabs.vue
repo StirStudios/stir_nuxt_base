855710b4af57d6631eebbd44bb5dc84368f5b5a3
Glyn Gray <glyn@stirstudiosdesign.com>
2025-01-17 14:20:03 -0800

    Added css via classes

--- app/components/Drupal/DrupalTabs.vue
+++ app/components/Drupal/DrupalTabs.vue
@@ -76,21 +76,14 @@ if (props.tabs.primary && props.tabs.primary.length > 0) {
 
 <template>
   <div
-    class="admin-links md:px-auto sticky top-0 w-full bg-zinc-200 px-4 px-8 text-black shadow shadow-gray-300 backdrop-blur-md dark:bg-gray-800 dark:text-white dark:shadow-gray-700"
+    class="md:px-auto sticky top-0 w-full bg-zinc-200 px-4 px-8 text-black shadow shadow-gray-300 backdrop-blur-md dark:bg-gray-800 dark:text-white dark:shadow-gray-700"
   >
     <UHorizontalNavigation
       :links="links"
       :ui="{
         base: 'text-xs',
+        label: 'truncate relative hidden md:block',
       }"
     />
   </div>
 </template>
-
-<style lang="css">
-.admin-links {
-  .truncate {
-    @apply hidden md:block;
-  }
-}
-</style>
