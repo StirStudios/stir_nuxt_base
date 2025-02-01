9c8f99963c6d4b9bca5d996e9073d7340c815a06
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-01 15:57:19 -0800

    Added import back

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { usePageContext } from '~/composables/usePageContext'
+
 const { fetchPage, renderCustomElements, usePageHead, getPageLayout } =
   await useDrupalCe()
 
