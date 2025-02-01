6621e436208059cc8669dfd66b0fb3603f088f53
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-01 15:27:25 -0800

    Simplifies and added some additional ADA

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,15 @@
 <template>
   <UApp>
-    <div class="transition">
-      <NuxtLoadingIndicator />
-      <a
-        href="#main-content"
-        class="bg-primary absolute left-0 top-0 z-50 m-3 -translate-y-96 p-3 text-black transition focus:translate-y-0"
-      >
-        Skip to main content
-      </a>
-      <NavigationMain />
-      <NuxtPage />
-      <LazyAppScrollToTop />
-    </div>
+    <NuxtLoadingIndicator />
+    <a
+      href="#main-content"
+      tabindex="0"
+      aria-label="Skip to main content"
+      class="bg-primary absolute top-0 left-0 z-50 m-3 -translate-y-96 p-3 text-black transition focus:translate-y-0"
+    >
+      Skip to main content
+    </a>
+    <NuxtPage />
+    <LazyAppScrollToTop />
   </UApp>
 </template>
-
-<style scroped>
-.page-enter-active,
-.page-leave-active {
-  @apply transition-all duration-500;
-}
-.page-enter-from,
-.page-leave-to {
-  @apply opacity-0;
-}
-</style>
