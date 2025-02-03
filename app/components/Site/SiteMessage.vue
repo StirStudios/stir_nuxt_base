a4137a7b3e74e544027af1d6764a0ce3535e1497
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-02 18:42:25 -0800

    Split messages up

---
+++ app/components/Site/SiteMessage.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  type: string
+  message: string
+}>()
+</script>
+
+<template>
+  <div :class="['message', type]">
+    <div v-html="message" />
+  </div>
+</template>
--- app/components/Site/SiteMessages.vue
+++ app/components/Site/SiteMessages.vue
@@ -3,34 +3,12 @@ const { getMessages } = useDrupalCe()
 const messages = getMessages()
 
 const dismiss = (index: number) => messages.value.splice(index, 1)
-</script>
 
-<template>
-  <div class="alerts">
-    <UAlert
-      v-for="(message, index) in messages"
-      :key="`${index}-${message.message}`"
-      :color="message.type || 'neutral'"
-      :title="getAlertTitle(message.type)"
-      icon="i-lucide-info"
-      close
-      @update:open="dismiss(index)"
-    >
-      <template #description>
-        <div v-html="message.message" />
-      </template>
-    </UAlert>
-  </div>
-</template>
-
-<script lang="ts">
-// Utility function for the alert title based on the message type
 function getAlertTitle(type: string): string {
   switch (type) {
     case 'success':
       return 'Success!'
     case 'error':
-    case 'danger':
       return 'Error!'
     case 'warning':
       return 'Warning!'
@@ -40,4 +18,37 @@ function getAlertTitle(type: string): string {
       return 'Notice'
   }
 }
+
+function getAlertIcon(type: string): string {
+  switch (type) {
+    case 'success':
+      return 'i-lucide-check-circle'
+    case 'error':
+      return 'i-lucide-x-circle'
+    case 'warning':
+      return 'i-lucide-alert-circle'
+    case 'info':
+      return 'i-lucide-info'
+    default:
+      return 'i-lucide-bell'
+  }
+}
 </script>
+
+<template>
+  <div class="alerts">
+    <UAlert
+      v-for="(message, index) in messages"
+      :key="`${index}-${message.message}`"
+      :color="message.type || 'neutral'"
+      :title="getAlertTitle(message.type)"
+      :icon="getAlertIcon(message.type)"
+      close
+      @update:open="dismiss(index)"
+    >
+      <template #description>
+        <SiteMessage :type="message.type" :message="message.message" />
+      </template>
+    </UAlert>
+  </div>
+</template>
