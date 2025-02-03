32093852676d50b4018b9521d475a3aedcd6061f
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-03 11:48:28 -0800

    Trans

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -7,17 +7,19 @@ const appConfig = useAppConfig()
 </script>
 
 <template>
-  <NuxtLayout :name="layout">
-    <template #default="{ page, renderCustomElements }">
-      <ParagraphHero
-        v-if="page.content.hero && page.content.hero.length > 0"
-        :hero="page.content.hero[0]"
-        :page-title="page.title"
-        :site-slogan="page.site_info.slogan"
-      />
-      <h1 v-else :class="appConfig.stirTheme.h1">{{ page.title }}</h1>
-      <LazySiteBreadcrumbs />
-      <component :is="renderCustomElements(page.content)" />
-    </template>
-  </NuxtLayout>
+  <div>
+    <NuxtLayout :name="layout">
+      <template #default="{ page, renderCustomElements }">
+        <ParagraphHero
+          v-if="page.content.hero && page.content.hero.length > 0"
+          :hero="page.content.hero[0]"
+          :page-title="page.title"
+          :site-slogan="page.site_info.slogan"
+        />
+        <h1 v-else :class="appConfig.stirTheme.h1">{{ page.title }}</h1>
+        <LazySiteBreadcrumbs />
+        <component :is="renderCustomElements(page.content)" />
+      </template>
+    </NuxtLayout>
+  </div>
 </template>
