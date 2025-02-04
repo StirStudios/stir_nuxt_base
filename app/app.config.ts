8aa8b7b3ec6eaca196880f83ef2c2daace59b930
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-03 18:51:46 -0800

    Added crumbs toggle

--- app/app.config.ts
+++ app/app.config.ts
@@ -6,6 +6,7 @@ export default defineAppConfig({
     preference: 'dark',
   },
   stirTheme: {
+    crumbs: false,
     loadingIndicator:
       'repeating-linear-gradient(to right,#D21B18 0%,#ED6663 50%,#F28E8D 100%)',
     h1: 'mb-20 text-center text-6xl',
