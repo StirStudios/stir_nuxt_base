19a05c4d6416dd686e3e276d17bd35eef3006a3b
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-06 12:19:27 -0800

    Stop toasts repeating

--- app/components/Site/SiteMessages.vue
+++ app/components/Site/SiteMessages.vue
@@ -1,17 +1,28 @@
 <script setup lang="ts">
+import { ref, onMounted, watchEffect } from 'vue'
 const toast = useToast()
 const { getMessages } = useDrupalCe()
+
+// State to track already shown messages
+const shownMessages = ref(new Set<string>())
 
 onMounted(() => {
   const messages = getMessages().value
 
   messages.forEach((message) => {
-    toast.add({
-      title: message.type === 'success' ? 'Success!' : 'Error!',
-      description: h('div', { innerHTML: message.message }),
-      icon: getAlertIcon(message.type),
-      color: message.type === 'success' ? 'success' : 'error',
-    })
+    // Check if the message has already been shown
+    if (!shownMessages.value.has(message.message)) {
+      // Show the toast
+      toast.add({
+        title: message.type === 'success' ? 'Success!' : 'Error!',
+        description: h('div', { innerHTML: message.message }),
+        icon: getAlertIcon(message.type),
+        color: message.type === 'success' ? 'success' : 'error',
+      })
+
+      // Track the shown message
+      shownMessages.value.add(message.message)
+    }
   })
 })
 
