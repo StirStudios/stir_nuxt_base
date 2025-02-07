e7f8d9a492515823b33c52ced61c2cbe3c569bc3
Glyn Gray <gg@ggmbp.local>
2025-02-06 17:43:01 -0800

    Removed incorrect class

--- app/components/Drupal/DrupalTabs.vue
+++ app/components/Drupal/DrupalTabs.vue
@@ -89,7 +89,7 @@ const links = computed(() => {
   <UNavigationMenu
     :items="links"
     :ui="{
-      root: 'dark:bg-neutral-000 sticky top-0 z-50 h-[3.1rem] w-full bg-neutral-200 dark:bg-neutral-900 p-4 shadow',
+      root: 'sticky top-0 z-50 h-[3.1rem] w-full bg-neutral-200 dark:bg-neutral-900 p-4 shadow',
       link: 'text-xs text-black dark:text-white',
       linkLabel: 'hidden md:block',
       linkLeadingIcon: 'text-black dark:text-white',
