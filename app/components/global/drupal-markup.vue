93a01e7f53610f55e5d05020a147e375bb835312
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-14 18:42:12 -0800

    Missed

---
+++ app/components/global/drupal-markup.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineSlots<{
+  default()
+}>()
+</script>
+
+<template>
+  <slot>
+    <component :is="useDrupalCe().renderCustomElements($attrs.content)" />
+  </slot>
+</template>
