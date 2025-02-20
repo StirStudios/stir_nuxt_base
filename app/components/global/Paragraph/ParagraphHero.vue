72e7f91c9b5a0664c76b66e7c3b4996eb59d22db
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-20 12:08:41 -0800

    Added prose

--- app/components/global/Paragraph/ParagraphHero.vue
+++ app/components/global/Paragraph/ParagraphHero.vue
@@ -46,7 +46,7 @@ const media = computed(() => hero.media?.[0] || {})
           <h1 v-else-if="pageTitle" class="mb-0 text-white">
             {{ pageTitle }}
           </h1>
-          <div v-if="hero.text" class="hero-copy" v-html="hero.text" />
+          <div v-if="hero.text" class="hero-copy prose" v-html="hero.text" />
         </WrapAnimate>
       </div>
       <img
