3fdd9c976984193683168b75380f7b35ec3d78e8
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-25 14:51:47 -0800

    Fixed heroContent

---
+++ app/components/Hero/HeroContent.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  pageTitle: String,
+  siteSlogan: String,
+  heroText: String,
+  isFront: Boolean,
+})
+</script>
+
+<template>
+  <template v-if="pageTitle && isFront">
+    <h1 class="sr-only">{{ pageTitle }}</h1>
+    <h2 class="display-h1 text-left">{{ siteSlogan }}</h2>
+  </template>
+  <h1 v-else-if="pageTitle" class="mb-0 text-white">
+    {{ pageTitle }}
+  </h1>
+  <div v-if="hero.text" class="hero-copy prose" v-html="hero.text" />
+</template>
