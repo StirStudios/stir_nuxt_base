c3d8e1528aae5690aab09ba9e76912921e8028a6
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-26 10:00:50 -0800

    H1 header

--- app/components/Hero/HeroContent.vue
+++ app/components/Hero/HeroContent.vue
@@ -9,7 +9,7 @@ defineProps({
 
 <template>
   <template v-if="pageTitle && isFront">
-    <h1 class="sr-only">{{ pageTitle }}</h1>
+    <h1>{{ pageTitle }}</h1>
     <h2 class="display-h1 text-left">{{ siteSlogan }}</h2>
   </template>
   <h1 v-else-if="pageTitle" class="mb-0 text-white">
