a66feec9b687dc64a593cf4b5cc54f683e003855
Glyn Gray <glyn@stirstudiosdesign.com>
2025-02-27 18:02:59 -0800

    Added highlight for menu active

--- app/components/Drupal/DrupalTabs.vue
+++ app/components/Drupal/DrupalTabs.vue
@@ -86,6 +86,8 @@ if (props.tabs.primary && props.tabs.primary.length > 0) {
         base: 'text-xs',
         label: 'truncate relative hidden md:block',
       }"
+      highlight
+      highlight-color="primary"
     />
   </div>
 </template>
