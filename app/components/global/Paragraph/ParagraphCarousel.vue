9aae5ba19f3678ed237670e1586c61bea9c513a5
Glyn Gray <glyn@stirstudiosdesign.com>
2025-03-27 17:41:34 -0700

    Edit carousel

--- app/components/global/Paragraph/ParagraphCarousel.vue
+++ app/components/global/Paragraph/ParagraphCarousel.vue
@@ -14,40 +14,42 @@ const itemElement = computed(() => props.itemElement || false)
 </script>
 
 <template>
-  <div :class="`relative z-10 ${appConfig.stirTheme.carousel.padding}`">
-    <h2 v-if="header" class="mb-5">{{ header }}</h2>
-    <UCarousel
-      v-slot="{ item }"
-      loop
-      :auto-scroll="autoscroll"
-      :fade="transitionFade"
-      :arrows="showArrows"
-      :dots="showIndicators"
-      :autoplay="{ delay: interval }"
-      :items="items"
-      :prev="appConfig.stirTheme.carousel.arrows.prev"
-      :next="appConfig.stirTheme.carousel.arrows.next"
-      :prev-icon="appConfig.stirTheme.carousel.arrows.prevIcon"
-      :next-icon="appConfig.stirTheme.carousel.arrows.nextIcon"
-      :ui="{
-        root: `${appConfig.stirTheme.carousel.root}`,
-        item: amount,
-      }"
-    >
-      <template v-if="itemElement">
-        <component
-          :is="
-            componentExists(itemElement)
-              ? resolveComponent(itemElement)
-              : 'ParagraphDefault'
-          "
-          :item="item"
-        />
-      </template>
-      <template v-else>
-        <MediaSimple :media="[item]" v-if="item.type === 'image'" />
-        <MediaPopup :media="[item]" v-else-if="item.type === 'video'" />
-      </template>
-    </UCarousel>
-  </div>
+  <EditLink :link="item.editLink">
+    <div :class="`relative z-10 ${appConfig.stirTheme.carousel.padding}`">
+      <h2 v-if="header" class="mb-5">{{ header }}</h2>
+      <UCarousel
+        v-slot="{ item }"
+        loop
+        :auto-scroll="autoscroll"
+        :fade="transitionFade"
+        :arrows="showArrows"
+        :dots="showIndicators"
+        :autoplay="{ delay: interval }"
+        :items="items"
+        :prev="appConfig.stirTheme.carousel.arrows.prev"
+        :next="appConfig.stirTheme.carousel.arrows.next"
+        :prev-icon="appConfig.stirTheme.carousel.arrows.prevIcon"
+        :next-icon="appConfig.stirTheme.carousel.arrows.nextIcon"
+        :ui="{
+          root: `${appConfig.stirTheme.carousel.root}`,
+          item: amount,
+        }"
+      >
+        <template v-if="itemElement">
+          <component
+            :is="
+              componentExists(itemElement)
+                ? resolveComponent(itemElement)
+                : 'ParagraphDefault'
+            "
+            :item="item"
+          />
+        </template>
+        <template v-else>
+          <MediaSimple :media="[item]" v-if="item.type === 'image'" />
+          <MediaPopup :media="[item]" v-else-if="item.type === 'video'" />
+        </template>
+      </UCarousel>
+    </div>
+  </EditLink>
 </template>
