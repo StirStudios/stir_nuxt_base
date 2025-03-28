4a6f3bbb103e4b9ad4698fa7572f8f7666f39eae
Glyn Gray <glyn@stirstudiosdesign.com>
2025-03-28 12:44:13 -0700

    Added button comp

---
+++ app/components/global/Paragraph/ParagraphButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import type { RegionItemProps } from '~/types/ContentTypes'
+
+const props = defineProps<{
+  item: RegionItemProps
+}>()
+
+const button = props.item.link?.[0] || null
+</script>
+
+<template>
+  <UButton
+    :label="button.title || 'Learn More'"
+    :to="button.uri"
+    class="mt-4"
+    target="_blank"
+  />
+</template>
