78c78fa0a3c331ce20b55139fbadc24db7436a40
Glyn Gray <glyn@stirstudiosdesign.com>
2025-03-29 11:45:06 -0700

    Added edit for button

--- app/components/global/Paragraph/ParagraphButton.vue
+++ app/components/global/Paragraph/ParagraphButton.vue
@@ -9,13 +9,15 @@ const button = props.item.link?.[0] || null
 </script>
 
 <template>
-  <div :class="['flex w-full', item.align, item.spacing, item.width]">
-    <UButton
-      v-if="button?.uri"
-      :label="button.title || 'Learn More'"
-      :to="button.uri"
-      class="mt-4"
-      target="_blank"
-    />
-  </div>
+  <EditLink :link="item.editLink">
+    <div :class="['flex w-full', item.align, item.spacing, item.width]">
+      <UButton
+        v-if="button?.uri"
+        :label="button.title || 'Learn More'"
+        :to="button.uri"
+        class="mt-4"
+        target="_blank"
+      />
+    </div>
+  </EditLink>
 </template>
