f694ff216e252cb323fff71fc40ce1c4959650e9
Glyn Gray <glyn@stirstudiosdesign.com>
2025-04-17 14:56:38 -0700

    Fix for label

--- app/components/Field/FieldTurnstile.vue
+++ app/components/Field/FieldTurnstile.vue
@@ -5,11 +5,16 @@ const config = useRuntimeConfig()
 const { turnstile: themeTurnstile = {} } = useAppConfig().stirTheme
 
 const showContainer = computed(() => !config.public.turnstileDisable)
+
+const showLabel = computed(
+  () =>
+    themeTurnstile.label && themeTurnstile.appearance !== 'interaction-only',
+)
 </script>
 
 <template>
   <div v-if="showContainer">
-    <p v-if="themeTurnstile.label" class="mb-2 text-sm text-(--ui-text-muted)">
+    <p v-if="showLabel" class="mb-2 text-sm text-(--ui-text-muted)">
       {{ themeTurnstile.label }}
     </p>
     <NuxtTurnstile
