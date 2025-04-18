7f8f89ca6e8a7d14917f7a2c6d363cae2256e0f8
Glyn Gray <glyn@stirstudiosdesign.com>
2025-04-18 10:08:25 -0700

    Plugin

--- app/composables/usePdfViewer.ts
+++ app/composables/usePdfViewer.ts
@@ -1,13 +1,4 @@
-export async function usePdfViewer() {
+export function usePdfViewer() {
   const licenseKey = useState<string>('pdf-license-key', () => '')
-
-  // Don't re-fetch if already set
-  if (!licenseKey.value && import.meta.client) {
-    const { data } = await useFetch<{ licenseKey: string }>(
-      '/api/vpv/license-key',
-    )
-    licenseKey.value = data.value?.licenseKey ?? ''
-  }
-
   return { licenseKey }
 }
