81e8709398460d09a722334131120c2eb5f128b8
Glyn Gray <glyn@stirstudiosdesign.com>
2025-04-25 18:37:48 -0700

    Reverted the PDF state

--- app/composables/usePdfViewer.ts
+++ app/composables/usePdfViewer.ts
@@ -1,12 +1,4 @@
-export async function usePdfViewer() {
+export function usePdfViewer() {
   const licenseKey = useState<string>('pdf-license-key', () => '')
-
-  if (!licenseKey.value) {
-    const { data } = await useFetch<{ licenseKey: string }>(
-      '/api/vpv/license-key',
-    )
-    licenseKey.value = data.value?.licenseKey ?? ''
-  }
-
   return { licenseKey }
 }
