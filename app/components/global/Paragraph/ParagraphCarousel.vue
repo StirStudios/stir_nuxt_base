33ea059c28e4591e1198938f1bf2b53806e099ce
Glyn Gray <glyn@stirstudiosdesign.com>
2025-06-04 09:08:14 -0700

    Better isolation for autoplay and autoscroll

--- app/components/global/Paragraph/ParagraphCarousel.vue
+++ app/components/global/Paragraph/ParagraphCarousel.vue
@@ -4,23 +4,40 @@ import { componentExists, resolveComponentName } from '~/utils/componentExists'
 
 const props = defineProps<CarouselProps>()
 
+const mounted = ref(false)
+onMounted(() => {
+  mounted.value = true
+})
+
 const appConfig = useAppConfig()
+
 const showIndicators = computed(() => props.indicators || false)
 const showArrows = computed(() => props.arrows || false)
 const transitionFade = computed(() => props.fade || false)
 const autoscroll = computed(() => props.autoscroll || false)
 const interval = computed(() => props.interval || 5000)
 const itemElement = computed(() => props.itemElement || false)
+
+const autoScrollOptions = computed(() =>
+  autoscroll.value ? { speed: 2, startDelay: 1000 } : false,
+)
+
+const autoplayOptions = computed(() =>
+  !autoscroll.value ? { delay: interval.value } : false,
+)
+
+const showCarousel = computed(() => mounted.value && props.items?.length > 0)
 </script>
 
 <template>
   <div :class="`relative z-10 ${appConfig.stirTheme.carousel.padding}`">
     <h2 v-if="header" class="mb-5">{{ header }}</h2>
     <UCarousel
+      v-if="showCarousel"
       v-slot="{ item }"
       :arrows="showArrows"
-      :auto-scroll="autoscroll"
-      :autoplay="{ delay: interval }"
+      :auto-scroll="autoScrollOptions"
+      :autoplay="autoplayOptions"
       :dots="showIndicators"
       :fade="transitionFade"
       :items="items"
@@ -30,7 +47,7 @@ const itemElement = computed(() => props.itemElement || false)
       :prev="appConfig.stirTheme.carousel.arrows.prev"
       :prev-icon="appConfig.stirTheme.carousel.arrows.prevIcon"
       :ui="{
-        root: `${appConfig.stirTheme.carousel.root}`,
+        root: appConfig.stirTheme.carousel.root,
         item: amount,
       }"
     >
