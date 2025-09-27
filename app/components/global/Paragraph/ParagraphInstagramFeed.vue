a4006f258d32d69b4e513d2cdfbd4cb3fcd6a6bd
Glyn Gray <glyn@stirstudiosdesign.com>
2025-09-26 23:48:24 -0700

    Fixed incorrect IG TS import

--- app/components/global/Paragraph/ParagraphInstagramFeed.vue
+++ app/components/global/Paragraph/ParagraphInstagramFeed.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import type { ViewItemProps } from '~/types/CustomElementTypes'
+import type { ViewItemProps } from '~/types/ViewTypes'
 
 defineProps<{ item: ViewItemProps }>()
 </script>
