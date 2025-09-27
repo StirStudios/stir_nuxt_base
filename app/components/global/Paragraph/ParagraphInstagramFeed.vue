f1c1cff51c5fcbe2364361fdec9b0c53c36b555d
Glyn Gray <glyn@stirstudiosdesign.com>
2025-09-26 23:46:12 -0700

    Removed media props

--- app/components/global/Paragraph/ParagraphInstagramFeed.vue
+++ app/components/global/Paragraph/ParagraphInstagramFeed.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import type { ViewItemProps } from '~/types/CustomElementTypes'
-import type { MediaProps } from '~/types/MediaTypes'
 
 defineProps<{ item: ViewItemProps }>()
 </script>
