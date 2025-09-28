2cd1b7d05bfac9f1788011d290ad622aeaa8fdae
Glyn Gray <glyn@stirstudiosdesign.com>
2025-09-28 14:38:35 -0700

    Fixed logo size props and added slot

--- app/components/App/AppLogo.vue
+++ app/components/App/AppLogo.vue
@@ -8,24 +8,31 @@ const { page } = usePageContext()
 
 const props = defineProps<{
   addClasses?: string
+  logoSize?: string
 }>()
 
-const svgClasses = computed(() =>
-  `${theme.navigation.logoSize} ${props.addClasses || ''} ${isDark.value ? 'fill-white' : 'fill-black'}`.trim(),
-)
+const svgClasses = computed(() => {
+  const size = props.logoSize || theme.navigation.logoSize || ''
+  return `${size} ${props.addClasses || ''} ${isDark.value ? 'fill-white' : 'fill-black'}`.trim()
+})
+
+const logoTitle = computed(() => page.site_info?.name ?? '')
 </script>
 
 <template>
-  Add SVG Logo Here
-  <!-- <svg
-    :class="svgClasses"
-    fill="currentColor"
-    height="100%"
-    :title="page.site_info?.name"
-    viewBox="0 0 2400 1560"
-    width="100%"
-    xml:space="preserve"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-  </svg> -->
+  <slot :class="svgClasses" :title="logoTitle">
+    Add SVG Logo Here
+    <!-- <svg
+      :class="svgClasses"
+      fill="currentColor"
+      height="100%"
+      :title="logoTitle"
+      viewBox="0 0 2400 1560"
+      width="100%"
+      xml:space="preserve"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect fill="currentColor" height="1560" width="2400" />
+    </svg> -->
+  </slot>
 </template>
