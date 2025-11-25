a2d89214f7dffd2d402f665d924daf520946fa68
Glyn Gray <glyn@stirstudiosdesign.com>
2025-11-24 22:54:52 -0800

    Added a default paragraph

---
+++ app/components/global/Paragraph/Default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const isDev = import.meta.dev
+
+const props = defineProps<{
+  id?: number | string
+  uuid?: string
+  parentUuid?: string
+  region?: string
+
+  editLink?: string
+}>()
+</script>
+
+<template>
+  <UAlert
+    v-if="isDev"
+    class="m-auto mb-20 max-w-lg space-y-2"
+    color="error"
+    title="Missing Paragraph Component"
+    variant="subtle"
+  >
+    <template #description>
+      <p class="mb-5">This paragraph does not have a mapped Vue component.</p>
+      <pre>{{ props }}</pre>
+    </template>
+  </UAlert>
+</template>
