ec40fc7ab607a1e5cd173126ffa1dec79f0c3943
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-02 14:58:04 -0800

    Added an auto install for nuxt UI when not present

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-12-01',
   css: ['~/assets/css/main.css'],
+  modules: [
+    function (_, nuxt) {
+      const modules = nuxt.options.modules.map((m) =>
+        typeof m === 'string' ? m : m[0],
+      );
+
+      if (!modules.includes('@nuxt/ui')) {
+        console.warn(
+          '[nuxtjs-drupal-stir-admin] @nuxt/ui not found — enabling automatically.',
+        );
+        nuxt.options.modules.unshift('@nuxt/ui');
+      }
+    },
+  ],
 });
