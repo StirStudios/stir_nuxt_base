7dde7c78cc8a7848fbde0577e9f129a5cecb4d18
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-03 13:49:55 -0800

    CSS resolve

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,12 @@
-// import { defineNuxtConfig } from 'nuxt/config';
-// import { createResolver, resolveModule } from '@nuxt/kit';
-//
-// const { resolve } = createResolver(import.meta.url);
+import { defineNuxtConfig } from 'nuxt/config';
+import { createResolver, resolveModule } from '@nuxt/kit';
+
+const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-12-01',
 
-  css: ['~/assets/css/main.css'],
+  css: [resolve('./app/assets/css/main.css')],
 
   modules: ['@nuxt/ui'],
 });
