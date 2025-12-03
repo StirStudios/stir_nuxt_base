468f4651877ade2702dbdfa82b2a4f00bce30cf3
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-02 18:46:08 -0800

    Idea

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,24 @@
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import { defineNuxtConfig } from 'nuxt/config';
+import { createResolver } from '@nuxt/kit';
 
-const currentDir = dirname(fileURLToPath(import.meta.url));
+const { resolve } = createResolver(import.meta.url);
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-12-01',
 
-  css: [join(currentDir, './app/assets/css/main.css')],
+  css: [resolve('./app/assets/css/main.css')],
 
   modules: [
-    // Register your own module so the layer runs!
-    join(currentDir, './node_modules/tailwindcss'),
-    join(currentDir, './node_modules/@nuxt/ui'),
+    function (_, nuxt) {
+      const modules = nuxt.options.modules.map((m) =>
+        typeof m === 'string' ? m : m[0],
+      );
+
+      // Auto-enable Nuxt UI
+      if (!modules.includes('@nuxt/ui')) {
+        console.warn('[admin-layer] enabling @nuxt/ui automatically');
+        nuxt.options.modules.unshift('@nuxt/ui');
+      }
+    },
   ],
 });
