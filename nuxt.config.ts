e8197e36e6e08d6f3d94ca1a992d9350b124f463
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-02 18:23:36 -0800

    Added a module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,19 +6,9 @@ const { resolve } = createResolver(import.meta.url);
 export default defineNuxtConfig({
   compatibilityDate: '2025-12-01',
 
+  // Only your CSS entry. Nothing else.
   css: [resolve('./app/assets/css/main.css')],
 
-  modules: [
-    function (_, nuxt) {
-      const modules = nuxt.options.modules.map((m) =>
-        typeof m === 'string' ? m : m[0],
-      );
-
-      // Auto-enable Nuxt UI
-      if (!modules.includes('@nuxt/ui')) {
-        console.warn('[admin-layer] enabling @nuxt/ui automatically');
-        nuxt.options.modules.unshift('@nuxt/ui');
-      }
-    },
-  ],
+  // Register your admin module (self layer)
+  modules: ['nuxtjs-drupal-stir-admin'],
 });
