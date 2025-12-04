61f676113f3f8d8225e9ee7fadbef981e6b9cb2b
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-04 09:20:57 -0800

    Internal check

--- app/components/Drupal/DrupalTabs.vue
+++ app/components/Drupal/DrupalTabs.vue
@@ -82,7 +82,7 @@ const links = computed(() => {
 
 <template>
   <UNavigationMenu
-    v-if="!isAdministrator"
+    v-if="isAdministrator"
     content-orientation="vertical"
     highlight
     highlight-color="primary"
