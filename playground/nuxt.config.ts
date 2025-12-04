007a72e40b040a63a15fe9ccd7cdc26a5ef09d32
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-03 21:31:46 -0800

    Removed runtime

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
   extends: [
@@ -11,15 +9,5 @@ export default defineNuxtConfig({
   drupalCe: {
     drupalBaseUrl: process.env.DRUPAL_URL,
     enableComponentPreview: false,
-  },
-  runtimeConfig: {
-    api: process.env.DRUPAL_URL,
-    apiKey: process.env.DRUPAL_API_KEY || '',
-    turnstile: {
-      secretKey: process.env.TURNSTILE_SECRET,
-    },
-    public: {
-      api: process.env.DRUPAL_URL,
-    },
   },
 });
