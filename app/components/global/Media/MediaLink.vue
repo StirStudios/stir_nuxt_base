9a5a5c3f520c109b82e9e1eb6d13621eded61943
Glyn Gray <glyn@stirstudiosdesign.com>
2025-12-16 19:55:46 -0800

    Added Media Link

---
+++ app/components/global/Media/MediaLink.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  type: 'link'
+  href: string
+  title?: string
+  target?: string
+}>()
+</script>
+
+<template>
+  <UButton
+    color="primary"
+    external
+    :label="title || 'Listen'"
+    :target="target || '_blank'"
+    :to="href"
+    variant="solid"
+  />
+</template>
