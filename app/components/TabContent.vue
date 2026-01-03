fca183bcd8c9c0ebbf0b5d1e313585cb92cddc64
Glyn Gray <glyn@stirstudiosdesign.com>
2026-01-03 14:58:53 -0800

    Added tab comps

---
+++ app/components/TabContent.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  // Identity
+  id?: number | string
+  uuid?: string
+  parentUuid?: string
+  region?: string
+
+  // Meta
+  editLink?: string
+}>()
+</script>
+
+<template>
+  <EditLink :link="editLink">
+    <slot />
+  </EditLink>
+</template>
